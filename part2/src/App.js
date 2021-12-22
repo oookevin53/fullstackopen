d407a87bb14a02a39d684f94507014c60e43ed61
Kevin <oookevin53@gmail.com>
2021-12-22 10:35:51 -0500

    submit examples

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
   const addNote = (event) => {
-    console.log(event)
     event.preventDefault()
     const noteObject = {
       content: newNote,
@@ -27,7 +36,6 @@ const App = (props) => {
   : notes.filter(note => note.important === true)
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './App.js'
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 )
