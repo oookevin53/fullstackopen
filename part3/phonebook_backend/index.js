1d98f997482ba6ffac04dac4a5bc88c6a2f92306
Kevin <oookevin53@gmail.com>
2022-01-15 16:55:04 -0500

    submit exercises 3.7 & 3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token("res-body", function (req, res) {
+  return JSON.stringify(req.body)
+})
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :res-body"))
 
 let persons = [
     { 
