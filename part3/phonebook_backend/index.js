3b25e0222b1f5b612af0dfb74d09f63bec8b1435
Kevin <oookevin53@gmail.com>
2022-01-16 16:48:52 -0500

    submit exercise 3.11

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -11,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :res-body"))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
