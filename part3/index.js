8667f31a7d467c573caa1bb928f95d40d9892e75
Kevin <oookevin53@gmail.com>
2022-01-15 20:52:11 -0500

    submit exercise 3.9

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method)
@@ -94,7 +98,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token("res-body", function (req, res) {
   return JSON.stringify(req.body)
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :res-body"))
+app.use(cors())
 
 let persons = [
     { 
